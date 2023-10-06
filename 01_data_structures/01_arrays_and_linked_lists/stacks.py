b43c6034d66c3164eb8b7755043549776251d074
Pedro <pedrofraz@gmail.com>
2023-10-06 10:21:08 +0100

    Stacks from linked lists.

--- 01_data_structures/01_arrays_and_linked_lists/stacks.py
+++ 01_data_structures/01_arrays_and_linked_lists/stacks.py
@@ -1,36 +1,54 @@
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
 class Stack:
+    """Implements a stack using linked lists."""
     
-    def __init__(self, initial_size = 10):
-        self.arr = [0 for _ in range(initial_size)]
-        self.next_index = 0
+    def __init__(self):
+        self.head = None
         self.num_elements = 0
         
-    def push(self, data):
-        if self.next_index == len(self.arr):
-            print("Out of space! Increasing array capacity ...")
-            self._handle_stack_capacity_full()
-        
-        self.arr[self.next_index] = data
-        self.next_index += 1
+    def push(self, value):
+        new_node = Node(value)
+        # if stack is empty
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head # place the new node at the head (top) of the linked list
+            self.head = new_node
+
         self.num_elements += 1
         
     def pop(self):
         if self.is_empty():
-            self.next_index = 0
-            return None
-        self.next_index -= 1
+            return
+        
+        value = self.head.value # copy data to a local variable
+        self.head = self.head.next # move head pointer to point to next node (top is removed by doing so)
         self.num_elements -= 1
-        return self.arr[self.next_index]
-
+        return value
+    
     def size(self):
         return self.num_elements
-
+    
     def is_empty(self):
         return self.num_elements == 0
-    
-    def _handle_stack_capacity_full(self):
-        old_arr = self.arr
 
-        self.arr = [0 for _ in range( 2* len(old_arr))]
-        for index, element in enumerate(old_arr):
-            self.arr[index] = element
+
+
+def to_list(stack):
+    """Gets the data from the nodes into a list."""
+    elements = []
+    i = 0
+    current = stack.head
+    while current:
+        elements.append(current.value)
+        current = current.next
+        i += 1
+        if i > stack.num_elements:
+            print("Iteration larger than number of elements.")
+            break
+    return elements
