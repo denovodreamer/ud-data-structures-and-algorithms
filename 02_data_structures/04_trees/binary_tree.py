5222a8396877e23d76c643e9eda842c7f865828e
denovodreamer <pedrofraz@gmail.com>
2023-12-12 14:00:32 +0100

    Binary tree.

--- 02_data_structures/04_trees/binary_tree.py
+++ 02_data_structures/04_trees/binary_tree.py
@@ -1,40 +1,9 @@
 
-from collections import deque
-
-
-
-# Queue to print the BST
-
-class Queue():
-    def __init__(self):
-        self.q = deque()
-        
-    def enq(self,value):
-        self.q.appendleft(value)
-        
-    def deq(self):
-        if len(self.q) > 0:
-            return self.q.pop()
-        else:
-            return None
-    
-    def __len__(self):
-        return len(self.q)
-    
-    def __repr__(self):
-        if len(self.q) > 0:
-            s = "<enqueue here>\n_________________\n" 
-            s += "\n_________________\n".join([str(item) for item in self.q])
-            s += "\n_________________\n<dequeue here>"
-            return s
-        else:
-            return "<queue is empty>"
-
 
 
 class Node:
         
-    def __init__(self,value = None):
+    def __init__(self, value=None):
         self.value = value
         self.left = None
         self.right = None
@@ -45,7 +14,7 @@ class Node:
     def get_value(self):
         return self.value
         
-    def set_left_child(self,node):
+    def set_left_child(self, node):
         self.left = node
         
     def set_right_child(self, node):
@@ -71,92 +40,12 @@ class Node:
     
 
 
-# Binary search tree
-
 class Tree():
     def __init__(self):
         self.root = None
         
-    def set_root(self,value):
+    def set_root(self, value):
         self.root = Node(value)
         
     def get_root(self):
         return self.root
-    
-    def compare(self, node, new_node):
-        """
-        0 means new_node equals node
-        -1 means new node less than existing node
-        1 means new node greater than existing node 
-        """
-        if new_node.get_value() == node.get_value():
-            return 0
-        elif new_node.get_value() < node.get_value():
-            return -1 # traverse left
-        else: # new_node > node
-            return 1  # traverse right
-    
-    def insert(self,new_value):
-        new_node = Node(new_value)
-
-        node = self.get_root()
-
-        # Check if tree is empty
-        if node is None: 
-            self.root = new_node
-            return
-        
-        node = self.root
-        while node is not None:
-            comparison = self.compare(node, new_node)
-            if comparison == -1:
-                if node.has_left_child():
-                    node = node.get_left_child()
-                else:
-                    node.set_left_child(new_node)
-                    return
-                    
-            if comparison == 1:
-                if node.has_right_child():
-                    node = node.get_right_child()
-                else:
-                    node.set_right_child(new_node)
-                    return
-                
-            if comparison == 0:
-                return
-
-    def __repr__(self):
-        level = 0
-        q = Queue()
-        visit_order = list()
-        node = self.get_root()
-        q.enq( (node,level) )
-        while(len(q) > 0):
-            node, level = q.deq()
-            if node == None:
-                visit_order.append( ("<empty>", level))
-                continue
-            visit_order.append( (node, level) )
-            if node.has_left_child():
-                q.enq( (node.get_left_child(), level +1 ))
-            else:
-                q.enq( (None, level +1) )
-
-            if node.has_right_child():
-                q.enq( (node.get_right_child(), level +1 ))
-            else:
-                q.enq( (None, level +1) )
-
-        s = "Tree\n"
-        previous_level = -1
-        for i in range(len(visit_order)):
-            node, level = visit_order[i]
-            if level == previous_level:
-                s += " | " + str(node) 
-            else:
-                s += "\n" + str(node)
-                previous_level = level
-
-                
-        return s
