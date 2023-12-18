730735c1111ef5c6f2da09cf8d63f1ee8951dc3e
denovodreamer <pedrofraz@gmail.com>
2023-12-18 14:18:07 +0100

    LRU cache.

--- 02_data_structures/project/solution/01_LRU_cache.py
+++ 02_data_structures/project/solution/01_LRU_cache.py
@@ -1,12 +1,5 @@
 
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-        self.previous = None
-
-
 class HashMap:
     def __init__(self, capacity):
         self.capacity = capacity
@@ -52,6 +45,14 @@ class HashMap:
 
 
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.previous = None
+
+
+
 class LRU_Cache(HashMap):
     def __init__(self, capacity):
         super().__init__(capacity)
@@ -69,6 +70,15 @@ class LRU_Cache(HashMap):
         
         return None
 
+    def remove(self, node):   
+        previous_node = node.previous
+        next_node = node.next
+
+        node.previous.next = next_node
+        node.next.previous = previous_node
+    
+        return None
+
     def remove_lru(self):
         new_tail = self.tail.next
         new_tail.previous = None
@@ -77,7 +87,10 @@ class LRU_Cache(HashMap):
         
         return None
     
-    def put(self, key, value):
+    def set(self, key, value):
+        if value is None:
+            return
+        
         node = Node(value)
 
         if not self.check_capacity():
@@ -89,6 +102,9 @@ class LRU_Cache(HashMap):
         self.num_entries += 1
 
     def get(self, key):
+        if key is None:
+            return 
+        
         # Get node from bucket array
         bucket_index = self.get_hash_code(key)
         node = self.bucket_array[bucket_index]
@@ -112,3 +128,47 @@ class LRU_Cache(HashMap):
             
     def __repr__(self):
         return str([v for v in self])
+
+    def to_list(self):
+        return [v for v in self]
+
+
+def test_lru():
+    cache = LRU_Cache(5)
+
+    cache.set(6, 6)
+    cache.set(7, 7)
+    cache.set(8, 8)
+
+    value = cache.get(7)
+
+    assert value == 7
+    assert cache.to_list() == [7, 8, 6]
+
+
+def test_cache_limit():
+    cache = LRU_Cache(5)
+
+    cache.set(6, 6)
+    cache.set(7, 7)
+    cache.set(8, 8)
+
+    _ = cache.get(7)
+
+    cache.set(10, 10)
+    cache.set(11, 11)
+    cache.set(12, 12) # The LRU element 6 is removed
+
+    assert cache.to_list() == [12, 11, 10, 7, 8]
+
+
+def test_null():
+    cache = LRU_Cache(5)
+    cache.set(1, None)
+    assert len(cache.to_list()) == 0
+
+# Tests
+if __name__ == "__main__":
+    test_lru()
+    test_cache_limit()
+    test_null()
---
+++ 02_data_structures/project/solution/01_LRU_cache_explanation.md
@@ -0,0 +1,5 @@
+
+
+A linked list is used to build the cache, along with a hash map in parallel.
+
+This way, the lookup operation takes O(1), and to insert and remove elements takes also O(1).
