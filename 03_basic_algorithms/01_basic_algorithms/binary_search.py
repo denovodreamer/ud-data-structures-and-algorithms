c43b5763614ed637c06a7f0b065e00dc29143258
denovodreamer <pedrofraz@gmail.com>
2023-12-18 17:01:02 +0100

    Basic algorithms.

---
+++ 03_basic_algorithms/01_basic_algorithms/binary_search.py
@@ -0,0 +1,74 @@
+
+
+
+def binary_search(array, target):
+    '''Binary search algorithm using iteration
+   
+    args:
+      array: a sorted array of items of the same type
+      target: the element you're searching for
+   
+    returns:
+      int: the index of the target, if found, in the source
+      -1: if the target is not found
+    '''
+    
+    start_index = 0
+    end_index = len(array) - 1
+    
+    while start_index <= end_index:
+        mid_index = (start_index + end_index)//2        # integer division in Python 3
+        
+        mid_element = array[mid_index]
+        
+        if target == mid_element:                       # we have found the element
+            return mid_index
+        
+        elif target < mid_element:                      # the target is less than mid element
+            end_index = mid_index - 1                   # we will only search in the left half
+        
+        else:                                           # the target is greater than mid element
+            start_index = mid_element + 1               # we will search only in the right half
+    
+    return -1
+
+
+
+def binary_search_recursive(array, target):
+    '''
+    This function will call `binary_search_recursive_soln` function.
+    You don't need to change this function.
+    
+    args:
+      array: a sorted array of items of the same type
+      target: the element you're searching for
+    '''
+    return binary_search_recursive_soln(array, target, 0, len(array) - 1)
+
+
+def binary_search_recursive_soln(array, target, start_index, end_index):
+    '''Write a function that implements the binary search algorithm using recursion
+    
+    args:
+      array: a sorted array of items of the same type
+      target: the element you're searching for
+      start_index: beginning of the index of the sub-arrays
+      end_index: end of the index of the sub-arrays
+         
+    returns:
+      int: the index of the target, if found, in the source
+      -1: if the target is not found
+    '''
+
+    if start_index > end_index:
+        return -1
+    
+    mid_index = (start_index + end_index)//2
+    mid_element = array[mid_index]
+    
+    if mid_element == target:
+        return mid_index
+    elif target < mid_element:
+        return binary_search_recursive_soln(array, target, start_index, mid_index - 1)
+    else:
+        return binary_search_recursive_soln(array, target, mid_index + 1, end_index)
---
+++ 03_basic_algorithms/project/01_square_root.py
@@ -0,0 +1,89 @@
+
+
+def sqrtSearch(low, high, N) :
+
+    # If the range is still valid
+    if (low <= high) :
+
+        # Find the mid-value of the range
+        mid = (low + high) // 2;
+
+        # Base Case
+        if ((mid * mid <= N) and ((mid + 1) * (mid + 1) > N)) :
+            return mid;
+
+        # Condition to check if the
+        # left search space is useless
+        elif (mid * mid < N) :
+            return sqrtSearch(mid + 1, high, N);
+
+        else :
+            return sqrtSearch(low, mid - 1, N);
+
+    return low;
+
+
+def sqrt(target):
+    """
+    Calculate the floored square root of a number
+
+    Args:
+       number(int): Number to find the floored squared root
+    Returns:
+       int: Floored Square Root
+    """
+
+    if target is None:
+        return
+
+    # Then initial range goes from 1 to the target
+    start = 1
+    end = target
+    middle = (start + end)//2  
+
+    while start < middle:
+
+        # Square value of the middle element
+        middle_squared = middle**2
+
+        if target == middle_squared: # Squared root was found
+            return middle
+        elif target < middle_squared:  # Move to the left part of the range
+            end = middle
+        else:   # Move to the right part of the range
+            start = middle
+
+        middle = (start + end)//2
+
+    return middle
+
+
+def test_integer_square_root():
+    """
+    Tests the case when the squared root is an integer value.
+    """
+    target = 25
+    assert sqrt(target) == 5
+
+
+def test_integer_square_root():
+    """
+    Tests the case when the squared root is a float value
+    and if the resulting value is the floor of the true value.
+    """
+    import math
+    target = 27
+    assert sqrt(target) == math.floor(math.sqrt(target))
+
+
+def test_null():
+    """
+    Tests the edge case when the input is null.
+    """
+    target = None
+    assert sqrt(target) is None
+
+if __name__ == "__main__":
+    test_integer_square_root()
+    test_integer_square_root()
+    test_null()
